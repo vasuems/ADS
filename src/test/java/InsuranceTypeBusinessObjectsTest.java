406471f7bb89b596088011858c4288a90d6cbb45
sprasad37 <sprasad37@CSCINDAI405298.asiapac.globalcsc.net>
2016-06-29 17:08:31 +0530

    Including failed testcase.

--- src/test/java/InsuranceTypeBusinessObjectsTest.java
+++ src/test/java/InsuranceTypeBusinessObjectsTest.java
@@ -27,7 +27,7 @@ public class InsuranceTypeBusinessObjectsTest {
 			  InsuranceTypeBusinessObject bobj = new InsuranceTypeBusinessObject();
 			  InsuranceType returnInsuranceType = null;
 			  InsuranceType insuranceType = new InsuranceType();
-			  insuranceType.setId(4);
+			  insuranceType.setId(2);
 			    try {
 			    	returnInsuranceType = bobj.Add(insuranceType);
 			    		//fail("Exception did not occur");
